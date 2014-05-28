854e323fabd787bd4e838a2876483643963364d1
John Doe <john@sample.com>
2014-05-28 00:34:21 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to ConverIt!");
+        System.out.println("**************************************************");
+        System.out.println("");
+        System.out.println("");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Please enter the country you wish to convert currency from:");
+        String baseCurrency = br.readLine();
+        System.out.println("Please enter the amount of money you wish to convert:");
+        String baseAmount = br.readLine();
+
+        System.out.println("Please enter the country you wish to convert currency to:");
+        String newCurrency = br.readLine();
+
+        System.out.println(baseCurrency);
+        System.out.println(baseAmount);
+        System.out.println(newCurrency);
+    }
+}
