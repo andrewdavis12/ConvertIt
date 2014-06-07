6e0ab42e6393c01da3b082a944422b8c6cdeab53
andrewdavis12 <drewd12@gmail.com>
2014-06-07 02:41:57 -0400

    New branch created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Welcome to ConverIt!");
+        System.out.println("Welcome to ConvertIt!");
         System.out.println("**************************************************");
         System.out.println("");
         System.out.println("");
@@ -25,7 +25,6 @@ public class Main {
         System.out.println(baseCurrency);
         System.out.println(baseAmount);
         System.out.println(newCurrency);
-        System.out.println();
         System.out.println();
     }
 }
